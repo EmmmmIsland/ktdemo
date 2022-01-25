6eab35a9199f83e7fc7eb08ebf0a9c52489b0c63
wangl <wangl@wangldeMacBook-Pro.local>
2022-01-25 17:29:45 +0800

    添加模糊页面

--- app/src/main/java/com/example/hellokt/ui/blur/BlurActivity.kt
+++ app/src/main/java/com/example/hellokt/ui/blur/BlurActivity.kt
@@ -1,34 +1,19 @@
 package com.example.hellokt.ui.blur
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.alibaba.android.arouter.launcher.ARouter
 import com.example.baseproject.base.BaseVmActivity
 import com.example.baseproject.router.ARouterConfig
-import com.example.hellokt.R
-import com.example.hellokt.bottomBar.BottomBarItemView
 import com.example.hellokt.databinding.ActivityBlurBinding
-import com.example.hellokt.databinding.ActivityMainBinding
-import com.example.hellokt.ui.homefragment.HomeFragment
-import com.example.hellokt.ui.minefragment.MineFragment
-import kotlinx.android.synthetic.main.activity_main.*
 
 @Route(path = ARouterConfig.AROUTER_PATH_BLUR_ACTIVITY)
 class BlurActivity : BaseVmActivity<ActivityBlurBinding>() {
-
-    fun start(context: Context) {
-        var intent = Intent(context, BlurActivity().javaClass)
-        context.startActivity(intent)
-    }
 
     override fun getLayout(): ActivityBlurBinding  = ActivityBlurBinding.inflate(layoutInflater)
 
     override fun initView(savedInstanceState: Bundle?) {
         super.initView(savedInstanceState)
-
+        binding.toolBar.setNavigationOnClickListener { finish() }
     }
 
     override fun initData() {
