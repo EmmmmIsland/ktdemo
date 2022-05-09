3b91c36a1afce7cafdfb963b12eacafd5390119c
MMM <mmm@MMMdeMacBook-Pro.local>
2022-05-09 16:52:10 +0800

    添加BottomNavigationView

--- app/src/main/java/com/example/hellokt/ui/navmain/NavMainActivity.kt
+++ app/src/main/java/com/example/hellokt/ui/navmain/NavMainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.hellokt.ui.navmain
 
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.example.baseproject.base.BaseVmActivity
 import com.example.baseproject.router.ARouterConfig
+import com.example.hellokt.R
 import com.example.hellokt.databinding.ActivityNavmainBinding
 
 @Route(path = ARouterConfig.AROUTER_PATH_NAV_MAIN_ACTIVITY)
@@ -12,5 +17,29 @@ class NavMainActivity : BaseVmActivity<ActivityNavmainBinding>(){
 
     override fun initView(savedInstanceState: Bundle?) {
         super.initView(savedInstanceState)
+        binding.navView.itemIconTintList = null
+
+        val navController = Navigation.findNavController(this, R.id.my_nav_host_fragment)
+        binding.navView.setupWithNavController(navController)
+
+//        binding.navView.setOnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.home -> {
+//                    Navigation.findNavController(this,R.id.my_nav_host_fragment).navigate(R.id.navHomeFragment)
+//                    return@setOnItemSelectedListener true
+//                }
+//                R.id.mid -> {
+//                    Navigation.findNavController(this,R.id.my_nav_host_fragment).navigate(R.id.navMidFragment)
+//
+//                    return@setOnItemSelectedListener true
+//                }
+//                R.id.mine -> {
+//                    Navigation.findNavController(this,R.id.my_nav_host_fragment).navigate(R.id.navMineFragment)
+//
+//                    return@setOnItemSelectedListener true
+//                }
+//            }
+//            return@setOnItemSelectedListener false
+//        }
     }
 }
--- common/network/src/main/java/com/hellokt/network/Api.kt
+++ common/network/src/main/java/com/hellokt/network/Api.kt
@@ -4,7 +4,7 @@ object Api {
     fun getBaseUrl(): String {
 //        return "https://localhost:8080/"
 //        return "https://192.168.0.101:8080/"
-        return "http://192.168.0.104:8080/"
+        return "http://192.168.0.103:8080/"
 //        return "https://www.github.com/"
     }
 }
